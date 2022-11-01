bbf71d95af804a6b394f4514db7a3d8a36176a43
keyan <weikeyan@zuoyebang.com>
2022-11-01 11:48:57 +0800

    1

--- Example/KLeaksFinder/KAppDelegate.m
+++ Example/KLeaksFinder/KAppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    // Override point for customization after application launch. 1
     return YES;
 }
 
